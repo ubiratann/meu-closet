a653485d1190894ca1f196db84a7019674f53eef
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-07-10 00:29:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Meu Closet',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
