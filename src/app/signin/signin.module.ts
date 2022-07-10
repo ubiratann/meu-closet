8a7e0744dd75208460f2768d3087f02628763c24
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-07-10 03:36:03 -0300

    Adiciona versao inicial do cadastro

--- src/app/signin/signin.module.ts
+++ src/app/signin/signin.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +8,36 @@ import { SigninPageRoutingModule } from './signin-routing.module';
 
 import { SigninPage } from './signin.page';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxMaskModule } from 'ngx-mask';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SigninPageRoutingModule
+    SigninPageRoutingModule,
+
+    MatExpansionModule,
+
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+
+    ReactiveFormsModule,
+
+    NgxMaskModule.forRoot()
   ],
   declarations: [SigninPage]
 })
--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-signin',
@@ -7,10 +10,36 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./signin.page.scss'],
 })
 export class SigninPage implements OnInit {
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
+  full_name = new FormControl('', [Validators.required]);
+  phone = new FormControl('', [Validators.required, Validators.pattern('^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$')])
 
-  constructor(private menuCtrl: MenuController) { }
+  constructor(
+    private menuCtrl: MenuController,
+    private router: Router) { }
+  hide = true;
 
   ngOnInit() {
     this.menuCtrl.enable(false);
+  }
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) 
+      return 'Você precisa inserir um email';
+    
+    return this.email.hasError('email') ? 'Email inválido!' : '';
+  }
+
+  getNameErrorMessage(){
+    return 'Você precisa inserir seu nome!';
+  }
+
+  getPhoneErrorMessage(){
+    if (this.email.hasError('required')) 
+      return 'Você precisa inserir um número de telefone!'
+    
+    return 'O formato do número é inválido!';
+  
   }
 }
