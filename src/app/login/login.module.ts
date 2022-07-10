d9fd9f5a457326ed4528b7637bf8beae25aa38d8
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-07-10 03:35:36 -0300

    Adiciona versao inicial da tela de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +8,33 @@ import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+
+    MatExpansionModule,
+
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+
+    ReactiveFormsModule
   ],
   declarations: [LoginPage]
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -8,10 +10,30 @@ import { MenuController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private menuCtrl: MenuController) { }
-
+  constructor(
+    private menuCtrl: MenuController,
+    private router: Router) { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+  
   ngOnInit() {
     this.menuCtrl.enable(false);
+  }
+
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Você precisa inserir um email';
+    }
+    return this.email.hasError('email') ? 'Email inválido!' : '';
+  }
+
+  login(){
+    this.router.navigate(['/folder'])
+  }
+
+  signin(){
+    this.router.navigateByUrl('/signin')
   }
 
 }
