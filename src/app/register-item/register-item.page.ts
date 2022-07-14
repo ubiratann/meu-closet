3f08e01fd95a912c533650a24e4b4dae061df2e4
Ubiratan Júnior <r14.ubiratanjunior@gmail.com>
2022-07-14 01:26:05 -0300

    Adiciona logica da pagina de registro de produtos

---
+++ src/app/register-item/register-item.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { CameraOptions } from '@awesome-cordova-plugins/camera';
+import { Camera } from '@awesome-cordova-plugins/camera/ngx';
+import { ToastController } from '@ionic/angular';
+import { Category } from 'src/models/category.model';
+import { Product } from 'src/models/product.model';
+
+@Component({
+  selector: 'app-register-item',
+  templateUrl: './register-item.page.html',
+  styleUrls: ['./register-item.page.scss'],
+})
+export class RegisterItemPage implements OnInit {
+
+  categories : Category[] = [];
+  description = new FormControl('', [Validators.required]);
+  image_uri: string = "../../assets/add-photo-icon-19.jpg";
+  product: Product;
+  sel_category: string;
+
+  cameraOptions: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+  }
+
+  constructor(
+    private camera: Camera,
+    private toastController: ToastController) { }
+
+  ngOnInit() {
+    this.categories = [
+      {id: 1, name: "Blusas"},
+      {id: 2, name: "Camisa de frio"},
+      {id: 3, name: "Roupas íntimas"},
+      {id: 4, name: "Acessórios"}
+    ]
+
+    this.product = new Product();
+    this.product.category = new Category();
+  }
+
+  getDescriptionErrorMessage(){
+    return 'Você precisa informar uma descrição!'; 
+  }
+
+  setProductPhoto(){
+    this.camera.getPicture(this.cameraOptions).then( image => {
+      console.log(image)
+      this.image_uri = 'data:image/jpeg;base64,' + image;
+    }).catch(error => {
+      this.toastController.create({
+        message: 'Erro na câmera!',
+        duration: 2000,
+        color: "danger"
+      }).then(toast => {
+        toast.present();
+      });
+    }); 
+  }
+}
